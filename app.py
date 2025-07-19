030706b7dc5ea74d804ac7b93d422acc369946aa
DenisSamorokov <dennis.samorokov@gmail.com>
2025-07-19 16:24:41 +0300

    translation update

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, request, session, redirect, url_for, render_template
 from flask_socketio import SocketIO
 from flask_login import LoginManager
 from flask_cors import CORS
+from translations import TRANSLATIONS
 import os
 from models.models import init_db, User, db
 from routes.routes import init_routes
@@ -15,6 +16,7 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'uxyn32814ex9d3_i23ur8iu2')
 app.config['SESSION_TYPE'] = 'filesystem'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///music_quiz.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SUPPORTED_LOCALES'] = ['ru', 'en', 'es']
 
 socketio = SocketIO(app, async_mode='eventlet', cors_allowed_origins="*", ping_timeout=10, ping_interval=5, max_http_buffer_size=1000000)
 CORS(app, resources={r"/*": {"origins": "*"}})
@@ -28,6 +30,20 @@ login_manager.login_view = 'login'
 @login_manager.user_loader
 def load_user(user_id):
     return db.session.get(User, int(user_id))
+
+def get_translations():
+    lang = session.get('lang', request.accept_languages.best_match(app.config['SUPPORTED_LOCALES']) or 'ru')
+    return TRANSLATIONS.get(lang, TRANSLATIONS['ru'])
+
+@app.context_processor
+def inject_translations():
+    return dict(t=get_translations(), get_locale=lambda: session.get('lang', request.accept_languages.best_match(app.config['SUPPORTED_LOCALES']) or 'ru'))
+
+@app.route('/set_language/<lang>')
+def set_language(lang):
+    if lang in app.config['SUPPORTED_LOCALES']:
+        session['lang'] = lang
+    return redirect(request.referrer or url_for('index'))
 
 init_routes(app, socketio)
 
